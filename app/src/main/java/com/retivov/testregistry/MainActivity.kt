95953ada352eac97fb0338500f1319ee662c3cec
sergey <baskervilei08@gmail.com>
2019-08-27 13:45:12 +0400

    init

---
+++ app/src/main/java/com/retivov/testregistry/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.retivov.testregistry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.retivov.testlogging.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
